1ee396d56b607722e6fc2d961adfc10fd69cde94
josep.pujol03@estudiant.upf.edu <josep.pujol03@estudiant.upf.edu>
2019-03-10 17:39:58 +0100

    three more additions to nodes and edges done

--- script.py
+++ script.py
@@ -32,10 +32,27 @@ with open(path+fileName, "r") as file:
         # Adding of the persons listed as representants in the database
         type1 = "person"
         newNode1 = node([row[28]+ " " + row[29]+" "+row[30], type1, None ,source ])
-        db.addNode(newNode1)
+        #db.addNode(newNode1)
 
         # Adding the private entities
         type2 = "priventity"
         newNode2=node([row[0], type2, row[10]+ "\n "+row[12], source])
-        db.addNode2(newNode2)
+        #db.addNode2(newNode2)
+
+        # Adding the adges of the relation between the relpresentatives and ther represented entities
+        #print("Initiate ADGES")
+        re1=[newNode1.name, newNode2.name]
+        #db.addEdge(re1)
+
+        # Extraction of social reason
+        type ="unknown"
+
+        newNode4=node([row[27],type, None, source ])
+        db.addNodeUnk(newNode4);
+
+        # Adding edge betwen social reason and enterprise
+
+        rel2=[newNode4.name, row[0]]
+        db.addEdgeSoc(rel2)
+
 
