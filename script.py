f2c88b154827e33227ce0c934a4d000019fe7134
josep.pujol03@estudiant.upf.edu <josep.pujol03@estudiant.upf.edu>
2019-03-10 13:08:59 +0100

    specified first two elements to load in the database

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from grupsDinteresExtractor import dbLoader
 import os
 import csv
+from  utils import node, edge
+
 
 
 
@@ -11,8 +13,29 @@ fileName="Registre_de_grups_d_inter_s_de_Catalunya.csv"
 path= os.path.dirname(os.path.abspath(__file__))+"/"
 
 
+
+
+
 with open(path+fileName, "r") as file:
+
+    # First look at the .csv to extract person ma erelatet to interest groups
+
+    source="IN"
+
     csv_reader = csv.reader(file)
     rows = list(csv_reader)
     headings=rows.pop(0)
-  
+    print(headings)
+
+    for row in rows:
+
+        # Adding of the persons listed as representants in the database
+        type1 = "person"
+        newNode1 = node([row[28]+ " " + row[29]+" "+row[30], type1, None ,source ])
+        db.addNode(newNode1)
+
+        # Adding the private entities
+        type2 = "priventity"
+        newNode2=node([row[0], type2, row[10]+ "\n "+row[12], source])
+        db.addNode2(newNode2)
+
