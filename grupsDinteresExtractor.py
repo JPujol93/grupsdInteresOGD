967116cb050ecf7c5da1b28252d9af2a78c9751b
josep.pujol03@estudiant.upf.edu <josep.pujol03@estudiant.upf.edu>
2019-03-10 17:38:40 +0100

    New methods of insertion added

--- grupsDinteresExtractor.py
+++ grupsDinteresExtractor.py
@@ -30,6 +30,23 @@ class dbLoader:
                 print(err)
 
 
+    def addNodeUnk(self, node):
+
+        queryExists = ("SELECT name, type  FROM nodes WHERE name=%s OR name =%s")
+        self.cursor.execute(queryExists, (node.name, "Hello"))
+
+        if len(self.cursor._rows) != 1:
+
+            queryAddNode = ("insert into nodes (name, type, extra, source) values(%s, %s, null, %s)")
+            self.cursor.execute(queryAddNode, (node.name, node.type, node.source))
+            self.connector.commit()
+
+        else:
+
+            print("This node already exists")
+
+
+
 
     def addNode(self, node):
 
@@ -40,7 +57,7 @@ class dbLoader:
         if len(self.cursor._rows) != 1:
 
             queryAddNode = ("insert into nodes (name, type, extra, source) values(%s, %s, null, %s)")
-            self.cursor.execute(queryAddNode, (node.name, node.type, node.extra, node.source))
+            self.cursor.execute(queryAddNode, (node.name, node.type, node.source))
             self.connector.commit()
 
         else:
@@ -67,3 +84,71 @@ class dbLoader:
 
             print("This node already exists")
 
+
+
+    def addEdge(self, edge):
+
+        queryGetID=("select id from nodes where name=%s or name =%s")
+        print(edge[0])
+        self.cursor.execute(queryGetID, (edge[0], edge[1]))
+        result =self.cursor.fetchall()
+        try:
+            for row in result:
+                print(row)
+
+            id1=result[0][0]
+            print(id1)
+            id2=result[1][0]
+            print(id2)
+            queryExists = ("SELECT origin, target, charge FROM edges WHERE origin=%s AND target =%s AND charge=%s")
+            self.cursor.execute(queryExists, (id1, id2, "respresentant"))
+            if len(self.cursor._rows) != 1 :
+                queryAddEdge=("insert into edges (origin, target, charge, source) values(%s, %s, %s, %s)")
+                self.cursor.execute(queryAddEdge, (id1, id2, "respresentant", "IN"))
+
+            self.connector.commit()
+        except Error as err:
+            print("DB ERROR ")
+        except IndexError as error:
+            print("INDEX ERROR")
+
+    def addEdgeSoc(self, edge):
+
+        queryGetID=("select id from nodes where name=%s or name =%s")
+        print(edge[0])
+        self.cursor.execute(queryGetID, (edge[0], edge[1]))
+        result =self.cursor.fetchall()
+        try:
+            if len(result)==1:
+                id1=result[0][0]
+                id2=id1
+            else:
+
+                for row in result:
+                    print(row)
+
+                id1=result[0][0]
+                print(id1)
+                id2=result[1][0]
+                print(id2)
+            queryExists = ("SELECT origin, target, charge FROM edges WHERE origin=%s AND target =%s AND charge=%s")
+            self.cursor.execute(queryExists, (id1, id2, "respresentant"))
+            if len(self.cursor._rows) != 1 :
+                queryAddEdge=("insert into edges (origin, target, charge, source) values(%s, %s, %s, %s)")
+                self.cursor.execute(queryAddEdge, (id1, id2, "respresentant", "IN"))
+
+            self.connector.commit()
+        except Error as err:
+            print("DB ERROR ")
+        except IndexError as error:
+            print("INDEX ERROR")
+
+    def isPerson(self, name ):
+
+        queryExists = ("SELECT name, type  FROM nodes WHERE name=%s AND type =%s")
+        self.cursor.execute(queryExists, (name, "person"))
+
+        if len(self.cursor._rows) == 0:
+            return False
+        else :
+            return True
